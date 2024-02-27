52e0f25aed5875640453a3247f63d310abb76a5e
jfietkau1 <120548552+jfietkau1@users.noreply.github.com>
2024-02-27 16:39:11 -0700

    homecontroller stuff for edit

--- Mission08_Team0315/Controllers/HomeController.cs
+++ Mission08_Team0315/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0315.Models;
 using System.Diagnostics;
+using Task = Mission08_Team0315.Models.Task;
 
 namespace Mission08_Team0315.Controllers
 {
@@ -37,8 +39,49 @@ namespace Mission08_Team0315.Controllers
 
             return RedirectToAction("Quadrants");
         }
+        public IActionResult AddTask()
+        {
 
 
+            return RedirectToAction("Quadrants");
+        }
 
+        public async Task<IActionResult> EditTask(Task taskToUpdate)
+        {
+            if (taskToUpdate == null || taskToUpdate.TaskId == 0)
+            {
+                return NotFound();
+            }
+
+            var existingTask = await _context.Tasks.FirstOrDefaultAsync(t => t.TaskId == taskToUpdate.TaskId);
+
+            if (existingTask != null)
+            {
+                existingTask.TaskName = taskToUpdate.TaskName;
+                existingTask.DueDate = taskToUpdate.DueDate;
+                existingTask.Quadrant = taskToUpdate.Quadrant;
+                existingTask.CategoryId = taskToUpdate.CategoryId;
+                existingTask.IsCompleted = taskToUpdate.IsCompleted;
+
+                try
+                {
+                    _context.Update(existingTask);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Tasks.Any(t => t.TaskId == taskToUpdate.TaskId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return RedirectToAction("Quadrants");
+        }
     }
 }
